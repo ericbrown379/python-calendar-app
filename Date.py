8b717c13509f4e74284799d68331f539eed642da
Mutammim <mutammimc@gmail.com>
2024-09-15 23:58:19 -0400

    temporary holder for GUI

---
+++ Date.py
@@ -0,0 +1,8 @@
+class Date:
+    #2D array storing time from and to, so if available from 1 to 2 and 4 to 9 it'd be [[1,2],[4,9]]
+    userAvailableTime = []
+
+    #How would the user choose to get time? 
+
+
+    
