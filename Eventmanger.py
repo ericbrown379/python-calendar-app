ae0cc1eb375a571d4ce51d353cdb674ce9ed3bf5
Eric Brown <ericbrown@Erics-MacBook-Air-4.local>
2024-09-18 23:58:05 -0400

    Created event class and structure

---
+++ Eventmanger.py
@@ -0,0 +1,16 @@
+class EventManger:
+    def __init__(self) -> None:
+        self.events - []
+
+    def add_event(self, event):
+        return
+    
+    def edit_event(self, event):
+        return
+    
+    def delete_event(self, event):
+        return
+    
+    def get_events_by_date(self, event):
+
+        return event
