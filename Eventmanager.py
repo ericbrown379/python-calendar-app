3a45087028d4a85338ed7c15c32f3928f1e27439
Eric Brown <ericbrown379@gmail.com>
2024-09-27 22:35:00 -0400

    event_managment_class

--- Eventmanager.py
+++ Eventmanager.py
@@ -1,16 +1,16 @@
 class EventManager:
-    def __init__(self) -> None:
+    def __init__(self):
         self.events = []
 
     def add_event(self, event):
         self.events.append(event)
-        
         return self.events
     
     def edit_event(self, event):
         return
     
     def delete_event(self, event):
+
         return
     
     def get_events_by_date(self, event):
